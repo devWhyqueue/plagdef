20047753446218810f66465dfcb7ddbd404e2d80
Yannik Queisler <dev.yannik.queisler@gmail.com>
2021-04-24 17:08:28 +0200

    Fix CI test

--- plagdef/tests/test_app.py
+++ plagdef/tests/test_app.py
@@ -1,15 +1,9 @@
 from click.testing import CliRunner
 
-from plagdef.app import cli, gui
+from plagdef.app import cli
 
 
 def test_cli_version():
     runner = CliRunner()
     result = runner.invoke(cli, ['--version'])
-    assert result.exit_code == 0
-
-
-def test_gui_version():
-    runner = CliRunner()
-    result = runner.invoke(gui, ['--version'])
     assert result.exit_code == 0
